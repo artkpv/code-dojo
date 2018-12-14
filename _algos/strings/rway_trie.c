1e1aad4ecff04213069f7cd42d2acff8b97fa2b1
AK <artyomkarpov@gmail.com>
2018-12-14 19:12:04 +0300

    save

--- _algos/strings/rway_trie.c
+++ _algos/strings/rway_trie.c
@@ -38,13 +38,20 @@ void Node_free(NodePtr trie) {
 }
 
 int _rt_chr_to_inx(char ch) {
-	return (int)(ch - 'a');
+	int inx = (int)(ch - 'a');
+	assert(0 <= inx);
+	assert(inx < RADIX);
+	return inx;
+}
+
+char _rt_inx_to_chr(int inx) {
+	return (char) ('a' + inx);
 }
 
 NodePtr _rt_put(NodePtr t, char * key, int keylen, int i, int value) {
 	if (t == NULL)
 		t = Node_ctr();
-	if (i == keylen - 1) {
+	if (i == keylen) {
 		t->value = value;
 		return t;
 	}
@@ -61,7 +68,7 @@ void rt_put(NodePtr t, char * key, int value) {
 int _rt_search(NodePtr t, char * key, int keylen, int i) {
 	if(t == NULL)
 		return NULLVALUE;
-	if (keylen - 1 == i)
+	if (keylen == i)
 		return t->value;
 	int inx = _rt_chr_to_inx(key[i]);
 	return _rt_search(t->next[inx], key, keylen, i + 1);
@@ -69,6 +76,55 @@ int _rt_search(NodePtr t, char * key, int keylen, int i) {
 
 int rt_search(NodePtr t, char * key) {
 	return _rt_search(t, key, strlen(key), 0);
+}
+
+int _rt_longest_prefix(NodePtr t, char * query, int i, int length, int qlen) {
+	if (t == NULL)
+		return length;
+	if (t->value != NULLVALUE)
+		length = i;
+	if (i == qlen)
+		return length;
+	int inx = _rt_chr_to_inx(query[i]);
+	if (t->next[inx] == NULL)
+		return length;	
+	return _rt_longest_prefix(t->next[inx], query, i + 1, length, qlen);
+}
+
+// Returns length of longest prefix key in the trie for the given query string.
+// Example: Trie: "a", "ab". Query: "ac". Result: 1
+int rt_longest_prefix(NodePtr t, char * query) {
+	int qlen = strlen(query);
+	if (qlen == 0)
+		return 0;
+	int inx = _rt_chr_to_inx(query[0]);
+	if (t == NULL)
+		return 0;
+	return _rt_longest_prefix(t->next[inx], query, 1, 0, qlen);
+}
+
+void _rt_print_inorder(NodePtr t, char * buffer, int len) {
+	if (t->value != NULLVALUE) {
+		char str[len + 1];
+		int i = -1;
+		while (++i < len)
+			str[i] = buffer[i];
+		str[i] = '\0';
+		printf("'%s': %d\n", str, t->value);
+	}
+	for (int i = 0; i < RADIX; i++) {
+		if (t->next[i] != NULL) {
+			buffer[len] = _rt_inx_to_chr(i);
+			_rt_print_inorder(t->next[i], buffer, len + 1);
+		}
+	}
+}
+
+void rt_print_inorder(NodePtr t) {
+	static int MAXSTRING = 999;
+	char * buffer = malloc(sizeof(char) * MAXSTRING);
+	_rt_print_inorder(t, buffer, 0);
+	free(buffer);
 }
 
 void main() {
@@ -85,6 +141,11 @@ void main() {
 	assert(rt_search(trie, "shell") == 11);
 	assert(rt_search(trie, "sea") == 12);
 
+	assert(rt_longest_prefix(trie, "shellsort") == 5);
+	assert(rt_longest_prefix(trie, "she") == 3);
+	assert(rt_longest_prefix(trie, "nonexistant") == 0);
+
+	rt_print_inorder(trie);
 	
 	Node_free(trie);
 	printf("all tests pass\n");
