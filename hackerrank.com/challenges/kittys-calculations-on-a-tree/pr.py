c4f9ad49f71bfed42c2238aef1448288de2499d0
AK <artyomkarpov@gmail.com>
2019-04-25 17:32:14 +0300

    save

--- hackerrank.com/challenges/kittys-calculations-on-a-tree/pr.py
+++ hackerrank.com/challenges/kittys-calculations-on-a-tree/pr.py
@@ -1,5 +1,7 @@
 #!python3
 """
+Next: see TODO
+
 Idea 1
 1) calc distance: n * (n-1)
 2) calc sum: n*(n-1)
@@ -14,6 +16,19 @@ For any two v, u nodes their distance will be:
     distance(v,u) = level(v) - parent(v,u) + level(u) - parent(v,u)
 Now how to traverse tree for this?
 
+
+Idea 3
+How to optimize sum of combinations multiplications ?
+Precompute all factors for all till 2*10^5. For each two numbers, get
+common divisors. ??? Extract most common de
+TODO
+
+
+Idea 4
+Two vertices, v and u on a tree. If on the same branch: level(b) - level(a).
+If on other branches: level(b) - 2 * level(parent(a,b)) + level(a).
+TODO
+Make balanced tree?
 
 """
 
@@ -40,10 +55,40 @@ class SP(object):  # shortest path
         self.distto = distto
 
 
+class TreeDistance(object):
+    """
+    TODO
+    Given n - root
+    Calc distance for any v, u on tree.
+    Idea: traverse tree, calc distance to previously visited vertices
+    from current:
+        distance(u,v) = distto(u, root) + level(v) - disto(parent(u,v), root)
+    where parent of u, v found from the upper stack till root.
+    Time: ~n*(n-1)/2, all pairs
+    Space: distto only, ~V
+    """
+    def __init__(self, tree):
+        self.tree = tree
+        size = tree.size
+        self.root = self.tree.size // 2  # root is unknown, so take any
+        self.distance = {}
+        self.order = []
+        self.inorder(self.root, set())
+
+    def inorder(node, parents):
+        pass
+        # TODO
+
+    def get_distance(self, v, u):
+        a = v if v < u else u
+        b = u if v < u else v
+        return self.distance[(a-1, b-1)]
+
+
 class Tree(object):
     def __init__(self, size):
         self.adj = [[] for _ in range(size)]
-        self._dist = {}
+        self.distance = {}
         self.size = size
 
     def add(self, a, b):
@@ -51,24 +96,22 @@ class Tree(object):
         self.adj[b-1] += [a-1]
 
     def calc_dist(self):
+        # td = TreeDistance(self)
+        # self.distance = td.distance
+
         for v in range(self.size):
             sp = SP(v, tree)
             for u, distance in enumerate(sp.distto):
                 a = v if v < u else u
                 b = u if v < u else v
-                if (a, b) not in self._dist:
-                    self._dist[(a, b)] = distance
-
-    def dist(self, v, u):
-        a = v if v < u else u
-        b = u if v < u else v
-        return self._dist[(a-1, b-1)]
+                if (a, b) not in self.distance:
+                    self.distance[(a, b)] = distance
 
 
 def calc(set_, tree):
     sum_ = 0
     for c in combinations(set_, 2):
-        sum_ += (c[0] * c[1] * tree.dist(c[0], c[1])) % MODULO
+        sum_ += (c[0] * c[1] * tree.get_distance(c[0], c[1])) % MODULO
         sum_ = (sum_ % MODULO)
     return sum_
 
