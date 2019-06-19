8d3653313573a3db9fce5d723b5fe1c6867c682a
AK <artyomkarpov@gmail.com>
2019-06-19 17:14:38 +0300

    save

--- clrs/15/15-4.py
+++ clrs/15/15-4.py
@@ -21,81 +21,86 @@ Time: 2^n (see 15.2 chapter)
 import unittest
 import functools
 
+INF = float('inf')
+
 
 def print_neatly(words, M):
-#     assert all(len(w) <= M for w in words)
-#
-#     @functools.lru_cache
-#     def count(i, j):
-#         if i == j:
-#             return M - len(words[i])
-#         min_spaces = float('inf')
-#         min_k = None
-#         for k in range(i, j + 1):
-#             left_spaces, left_breaks = count(i, k)
-#             right_spaces, right_breaks = count(i, k)
-#
-#
-#
-#     breaks = {}
-#     count = {}
-#     # put 1 words per line:
-#     for i, w in enumerate(words):
-#         count[(i, i)] = M - len(w)
-#
-#     # put m words per line:
-#     for m in range(2, len(words)):
-#         for i in range(len(words) - m + 1):
-#             for k in range(i, i+m+1):
-#
-#                 spaces = (M
-#                         - sum(len(w) for w in words[i:i+m])
-#                         - m + 1
-#             count[(i,i+m-1)] = spaces
-#
-    # construct out from breaks
-    # count[0, len(words)-1]
-    raise Exception('not implemented')
+    """
+    Get how to print neatly words.
+    @words: array of words.
+    @M: line width.
+    Returns: cost to print, array of breaks.
+    """
+    assert all(len(w) <= M for w in words)
+    def extra(i, j):
+        """ Extra spaces to print words at i+1..j. """
+        return M - (j - i - 1) - sum(len(w) for w in words[i+1:j+1])
+
+    cost = [INF] * (len(words)+1)  # Cost to optimally print words.
+    breaks = [None] * len(words)  # Rightmost break at word for words.
+    cost[0] = 0
+    for j in range(1, len(words)+1):
+        for i in range(j):
+            spaces = extra(i-1, j-1)
+            if j == len(words) and spaces >= 0:  # last
+                spaces = 0
+            elif spaces < 0:
+                spaces = INF
+            spaces += cost[i]
+            if spaces < cost[j]:
+                cost[j] = spaces
+                breaks[j-1] = i-1
+    return cost[len(words)], breaks
 
 
-def count_neatlieness(text):
-    """ Counts neatless """
-    count = 0
-    if not text:
-        return count
-    for line in text.split('\n'):
-        count += len(line) - 1 - next(
-            (i for i, e in enumerate(reversed(line))
-             if e != ' '), -1)
-    return count
+def line_neatlieness(line):
+    return len(line) - 1 - next(
+        (i for i, e in enumerate(reversed(line))
+         if e != ' '), -1)
+
+
+def join_words(words, breaks, line_width):
+    text = []
+    j = len(words)-1
+    while j >= 0:
+        i = breaks[j]
+        line = ' '.join(words[i+1:j+1])
+        assert len(line) <= line_width
+        line += ' ' * (line_width - len(line))
+        text += [line]
+        j = i
+    return '\n'.join(reversed(text))
 
 
 class Tests(unittest.TestCase):
     """ UT """
     def test_count_neatlieness(self):
-        self.assertEqual(count_neatlieness('abc  '), 2)
-        self.assertEqual(count_neatlieness('  '), 2)
-        self.assertEqual(count_neatlieness(''), 0)
-        self.assertEqual(count_neatlieness('abc  \ndef  '), 4)
+        self.assertEqual(line_neatlieness('abc  '), 2)
+        self.assertEqual(line_neatlieness('  '), 2)
+        self.assertEqual(line_neatlieness(''), 0)
 
     def test_4_by_3(self):
         words = ['abc','def','ghi','jkl']
-        actual = print_neatly(words, 4)
-        actual_empty = count_neatlieness(actual)
-        expected = "abc \ndef \nghi \njkl \n"
-        expected_empty = count_neatlieness(expected)
-        self.assertEqual(actual_empty, expected_empty)
+        line_width = 4
+        count, breaks = print_neatly(words, line_width)
+        actual = join_words(words, breaks, line_width)
+        expected = "abc \ndef \nghi \njkl "
+        expected_empty = 3
+        self.assertEqual(actual, expected)
+        self.assertEqual(count, expected_empty)
 
     def test_three(self):
-"""
-      v
-1 2 3__
-5 6 77_
-99999 c
-3
+        expected = "aaaa bb  \ncc dddd  \neee fffff"
+        expected_empty = 4
+        width = 9
+        words = []
+        for line in expected.split('\n'):
+            words += [w.strip() for w in line.strip().split(' ') if w]
+        count, breaks = print_neatly(words, width)
+        actual = join_words(words, breaks, width)
+        self.assertEqual(actual, expected)
+        self.assertEqual(count, expected_empty)
 
-
-"""
 
 
 if __name__ == '__main__':
