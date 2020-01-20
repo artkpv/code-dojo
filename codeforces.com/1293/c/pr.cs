32a60f0ba5762a7aeddc92e60460695d69fe6010
AK <artyomkarpov@gmail.com>
2020-01-20 12:41:41 +0300

    save

---
+++ codeforces.com/1293/c/pr.cs
@@ -0,0 +1,100 @@
+#define TRACE
+#undef DEBUG
+/*
+Author: w1ld [dog] inbox [dot] ru
+
+ */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using static System.Math;
+
+public class Solver
+{
+    public void Solve()
+    {
+        int n = ReadInt();
+        int tests = ReadInt();
+        int count = 0;
+        bool[][] a = new bool[2][];
+        for (int i = 0; i < 2; i++)
+            a[i] = new bool[n];
+        for (int test = 0; test < tests; test++)
+        {
+            int r = ReadInt();
+            int c = ReadInt();
+            r -= 1;
+            c -= 1;
+            
+            int rr = (r + 1) % 2;
+            a[r][c] = !a[r][c];
+            for (int i = -1; i <= 1; i++)
+            {
+                if (c + i < 0 
+                    || c + i >= n 
+                    || (rr == 0 && c + i == 0) 
+                    || (rr == 1 && c + i == n - 1))
+                    continue;
+                if (a[rr][c+i])
+                    count += (a[r][c] ? 1 : -1);
+            }
+            Write(count > 0 ? "No" : "Yes");
+        }
+    }
+
+    #region Main
+
+    protected static TextReader reader;
+    protected static TextWriter writer;
+    static void Main()
+    {
+        Debug.Listeners.Clear();
+        Debug.Listeners.Add(new ConsoleTraceListener());
+        Trace.Listeners.Clear();
+        Trace.Listeners.Add(new ConsoleTraceListener());
+
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+        new Solver().Solve();
+        reader.Close();
+        writer.Close();
+    }
+
+    #endregion
+
+    #region Read / Write
+    private static Queue<string> currentLineTokens = new Queue<string>();
+    private static string[] ReadAndSplitLine() { return reader.ReadLine().Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries); }
+    public static string ReadToken() { while (currentLineTokens.Count == 0)currentLineTokens = new Queue<string>(ReadAndSplitLine()); return currentLineTokens.Dequeue(); }
+    public static int ReadInt() { return int.Parse(ReadToken()); }
+    public static long ReadLong() { return long.Parse(ReadToken()); }
+    public static double ReadDouble() { return double.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+    public static int[] ReadIntArray() { return ReadAndSplitLine().Select(int.Parse).ToArray(); }
+    public static long[] ReadLongArray() { return ReadAndSplitLine().Select(long.Parse).ToArray(); }
+    public static double[] ReadDoubleArray() { return ReadAndSplitLine().Select(s => double.Parse(s, CultureInfo.InvariantCulture)).ToArray(); }
+    public static int[][] ReadIntMatrix(int numberOfRows) { int[][] matrix = new int[numberOfRows][]; for (int i = 0; i < numberOfRows; i++)matrix[i] = ReadIntArray(); return matrix; }
+    public static int[][] ReadAndTransposeIntMatrix(int numberOfRows)
+    {
+        int[][] matrix = ReadIntMatrix(numberOfRows); int[][] ret = new int[matrix[0].Length][];
+        for (int i = 0; i < ret.Length; i++) { ret[i] = new int[numberOfRows]; for (int j = 0; j < numberOfRows; j++)ret[i][j] = matrix[j][i]; } return ret;
+    }
+    public static string[] ReadLines(int quantity) { string[] lines = new string[quantity]; for (int i = 0; i < quantity; i++)lines[i] = reader.ReadLine().Trim(); return lines; }
+    public static void WriteArray<T>(IEnumerable<T> array) { writer.WriteLine(string.Join(" ", array)); }
+    public static void Write(params object[] array) { WriteArray(array); }
+    public static void WriteLines<T>(IEnumerable<T> array) { foreach (var a in array)writer.WriteLine(a); }
+    private class SDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        public new TValue this[TKey key]
+        {
+            get { return ContainsKey(key) ? base[key] : default(TValue); }
+            set { base[key] = value; }
+        }
+    }
+    private static T[] Init<T>(int size) where T : new() { var ret = new T[size]; for (int i = 0; i < size; i++)ret[i] = new T(); return ret; }
+    #endregion
+}
