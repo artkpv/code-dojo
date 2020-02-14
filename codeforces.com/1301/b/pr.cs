cca328bf104889a6a9dae4ece647b6190466a5c5
AK <artyomkarpov@gmail.com>
2020-02-14 10:56:15 +0300

    save

---
+++ codeforces.com/1301/b/pr.cs
@@ -0,0 +1,106 @@
+#define TRACE
+#undef DEBUG
+/*
+Author: w1ld [at] inbox [dot] ru
+
+ */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using static System.Math;
+
+public class Solver
+{
+    public void Solve()
+    {
+        int tests = ReadInt();
+        for (int test = 0; test < tests; test++)
+        {
+            int n = ReadInt();
+            int[] arr = ReadIntArray();
+            int min = int.MaxValue;
+            int max = -1;
+            int dev = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] == -1 && arr[i-1] != -1)
+                {
+                    min = Min(arr[i-1], min);
+                    max = Max(arr[i-1], max);
+                }
+                else if (arr[i] != -1 && arr[i-1] == -1)
+                {
+                    min = Min(arr[i], min);
+                    max = Max(arr[i], max);
+                }
+                else if (arr[i] != -1 && arr[i-1] != -1)
+                {
+                    dev = Max(dev, Abs(arr[i] - arr[i-1]));
+                }
+            }
+
+            int x = (min + max) / 2;
+            dev = Max(dev, Max(x - min, max - x));
+            if (min == int.MaxValue)
+                x = 42;
+            Write($"{dev} {x}");
+
+        }
+    }
+
+    #region Main
+
+    protected static TextReader reader;
+    protected static TextWriter writer;
+    static void Main()
+    {
+        Debug.Listeners.Clear();
+        Debug.Listeners.Add(new ConsoleTraceListener());
+        Trace.Listeners.Clear();
+        Trace.Listeners.Add(new ConsoleTraceListener());
+
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+        new Solver().Solve();
+        reader.Close();
+        writer.Close();
+    }
+
+    #endregion
+
+    #region Read / Write
+    private static Queue<string> currentLineTokens = new Queue<string>();
+    private static string[] ReadAndSplitLine() { return reader.ReadLine().Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries); }
+    public static string ReadToken() { while (currentLineTokens.Count == 0)currentLineTokens = new Queue<string>(ReadAndSplitLine()); return currentLineTokens.Dequeue(); }
+    public static int ReadInt() { return int.Parse(ReadToken()); }
+    public static long ReadLong() { return long.Parse(ReadToken()); }
+    public static double ReadDouble() { return double.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+    public static int[] ReadIntArray() { return ReadAndSplitLine().Select(int.Parse).ToArray(); }
+    public static long[] ReadLongArray() { return ReadAndSplitLine().Select(long.Parse).ToArray(); }
+    public static double[] ReadDoubleArray() { return ReadAndSplitLine().Select(s => double.Parse(s, CultureInfo.InvariantCulture)).ToArray(); }
+    public static int[][] ReadIntMatrix(int numberOfRows) { int[][] matrix = new int[numberOfRows][]; for (int i = 0; i < numberOfRows; i++)matrix[i] = ReadIntArray(); return matrix; }
+    public static int[][] ReadAndTransposeIntMatrix(int numberOfRows)
+    {
+        int[][] matrix = ReadIntMatrix(numberOfRows); int[][] ret = new int[matrix[0].Length][];
+        for (int i = 0; i < ret.Length; i++) { ret[i] = new int[numberOfRows]; for (int j = 0; j < numberOfRows; j++)ret[i][j] = matrix[j][i]; } return ret;
+    }
+    public static string[] ReadLines(int quantity) { string[] lines = new string[quantity]; for (int i = 0; i < quantity; i++)lines[i] = reader.ReadLine().Trim(); return lines; }
+    public static void WriteArray<T>(IEnumerable<T> array) { writer.WriteLine(string.Join(" ", array)); }
+    public static void Write(params object[] array) { WriteArray(array); }
+    public static void WriteLines<T>(IEnumerable<T> array) { foreach (var a in array)writer.WriteLine(a); }
+    private class SDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        public new TValue this[TKey key]
+        {
+            get { return ContainsKey(key) ? base[key] : default(TValue); }
+            set { base[key] = value; }
+        }
+    }
+    private static T[] Init<T>(int size) where T : new() { var ret = new T[size]; for (int i = 0; i < size; i++)ret[i] = new T(); return ret; }
+    #endregion
+}
---
+++ codeforces.com/1301/c/pr.cs
@@ -0,0 +1,111 @@
+#define TRACE
+#define DEBUG
+/*
+Author: w1ld [at] inbox [dot] ru
+
+ */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using static System.Math;
+
+public class Solver
+{
+    public void Solve()
+    {
+        int tests = ReadInt();
+        for (int test = 0; test < tests; test++)
+        {
+            int n = ReadInt();
+            int m = ReadInt();
+            if (m == 0)
+            {
+                Write(0);
+            }
+            else 
+            {
+                //   n - len
+                //   m - num of 1s
+                //   m+1 - intervals of 0z
+                //
+                long l = (n - m) / (m + 1L);
+                long lg = (n - m) % (m + 1L);
+
+                long x = (m + 1L - lg) * (l * (l + 1L) / 2L);
+                long y = lg * (l + 1L) * (l + 2L) / 2L;
+                long ans = (n*(n+1L)) / 2L - x - y;
+                Write(ans);
+
+                //
+                //  9 3 ->   010010010
+                //  
+                //  6 / 4 - 1
+                //  lg = 2
+                //  ans - (4-2) * 1 - 2 * (2*3 / 2) = ans - 2 - 2 * 3 = 45 - 8 = 37
+                //
+                //
+                //  9 7
+                //  l = 2 / 8 = 0
+                //  lg = 2
+                //  45 - (8-2) * 0 - 2 * (1 * 2 / 2 ) = 43
+                //  
+            }
+        }
+    }
+
+    #region Main
+
+    protected static TextReader reader;
+    protected static TextWriter writer;
+    static void Main()
+    {
+        Debug.Listeners.Clear();
+        Debug.Listeners.Add(new ConsoleTraceListener());
+        Trace.Listeners.Clear();
+        Trace.Listeners.Add(new ConsoleTraceListener());
+
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+        new Solver().Solve();
+        reader.Close();
+        writer.Close();
+    }
+
+    #endregion
+
+    #region Read / Write
+    private static Queue<string> currentLineTokens = new Queue<string>();
+    private static string[] ReadAndSplitLine() { return reader.ReadLine().Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries); }
+    public static string ReadToken() { while (currentLineTokens.Count == 0)currentLineTokens = new Queue<string>(ReadAndSplitLine()); return currentLineTokens.Dequeue(); }
+    public static int ReadInt() { return int.Parse(ReadToken()); }
+    public static long ReadLong() { return long.Parse(ReadToken()); }
+    public static double ReadDouble() { return double.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+    public static int[] ReadIntArray() { return ReadAndSplitLine().Select(int.Parse).ToArray(); }
+    public static long[] ReadLongArray() { return ReadAndSplitLine().Select(long.Parse).ToArray(); }
+    public static double[] ReadDoubleArray() { return ReadAndSplitLine().Select(s => double.Parse(s, CultureInfo.InvariantCulture)).ToArray(); }
+    public static int[][] ReadIntMatrix(int numberOfRows) { int[][] matrix = new int[numberOfRows][]; for (int i = 0; i < numberOfRows; i++)matrix[i] = ReadIntArray(); return matrix; }
+    public static int[][] ReadAndTransposeIntMatrix(int numberOfRows)
+    {
+        int[][] matrix = ReadIntMatrix(numberOfRows); int[][] ret = new int[matrix[0].Length][];
+        for (int i = 0; i < ret.Length; i++) { ret[i] = new int[numberOfRows]; for (int j = 0; j < numberOfRows; j++)ret[i][j] = matrix[j][i]; } return ret;
+    }
+    public static string[] ReadLines(int quantity) { string[] lines = new string[quantity]; for (int i = 0; i < quantity; i++)lines[i] = reader.ReadLine().Trim(); return lines; }
+    public static void WriteArray<T>(IEnumerable<T> array) { writer.WriteLine(string.Join(" ", array)); }
+    public static void Write(params object[] array) { WriteArray(array); }
+    public static void WriteLines<T>(IEnumerable<T> array) { foreach (var a in array)writer.WriteLine(a); }
+    private class SDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        public new TValue this[TKey key]
+        {
+            get { return ContainsKey(key) ? base[key] : default(TValue); }
+            set { base[key] = value; }
+        }
+    }
+    private static T[] Init<T>(int size) where T : new() { var ret = new T[size]; for (int i = 0; i < size; i++)ret[i] = new T(); return ret; }
+    #endregion
+}
